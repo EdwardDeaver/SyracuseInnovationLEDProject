5229eb3991d024ec62a7ce8820a10eb26756f6c2
Edward Deaver <edwardcdeaveriv@gmail.com>
2019-12-12 19:36:42 -0500

    Now calls the programs based on current directory

--- LocalHost/startProgram.sh
+++ LocalHost/startProgram.sh
@@ -3,7 +3,16 @@
 # Uses caffinate to force conputer to not sleep
 # If using non-mac os system comment out caffeinate
 echo "Starting OpenFrameworks App, then UDP Server"
-open OpenFrameworks/apps/myApps/PublicInstallationProject/bin/PublicInstallationProjectDebug.app 
-python3 python/UDPSERVER.py
+#GETS CURRENT DIRECTORY
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )" 
+# SETS OPENFRAMEWORKS APP PATH
+OPENFRAMEWORKSPATH="/OpenFrameworks/apps/myApps/PublicInstallationProject/bin/PublicInstallationProjectDebug.app"
+# SETS PYTHON APP PATH
+PYTHONAPPPATH="/python/UDPSERVER.py"
+#OPENS OPENFRAMEWORKS APP
+open "$SCRIPTPATH$OPENFRAMEWORKSPATH"
+#RUNS PYTHON3 APP 
+python3 "$SCRIPTPATH$PYTHONAPPPATH"
+#FORCES MAC TO NOT SLEEP
 caffeinate
 
